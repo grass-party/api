11ba326672977377276a87220f445ec845510a9e
qodot <waitingforqodot@gmail.com>
2017-12-22 14:05:45 +0900

    Fix dockerfile/composefile / Add django db setting

--- gpapi/settings.py
+++ gpapi/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'gpapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': 5432,
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
     }
 }
 
