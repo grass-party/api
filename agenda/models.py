540abb283b141478d0e63195bb2e5356adad1636
qodot <waitingforqodot@gmail.com>
2018-01-20 21:54:15 +0900

    Add order column to choice & Add unique constraint to title

--- agenda/models.py
+++ agenda/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Agenda(models.Model):
     owner = models.ForeignKey(get_user_model(), related_name='agendas',
                               on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, unique=True)
     description = models.TextField()
     is_publish = models.BooleanField(default=False)
     published_at = models.DateTimeField(blank=True, null=True)
@@ -16,13 +16,14 @@ class Agenda(models.Model):
 class Choice(models.Model):
     agenda = models.ForeignKey('Agenda', related_name='choices',
                                on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, unique=True)
+    order = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
 class Tag(models.Model):
-    title = models.CharField(max_length=30)
     agendas = models.ManyToManyField('Agenda')
+    title = models.CharField(max_length=30, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
