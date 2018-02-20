66151122037fda5e5249c8bd3455bea668b43fd7
최민호 <vnthf1@naver.com>
2018-02-20 09:16:30 +0900

    Add allow host

--- gpapi/settings/dev.py
+++ gpapi/settings/dev.py
@@ -1,4 +1,4 @@
 from gpapi.settings.base import *
 
 
-ALLOWED_HOSTS = ['w0r94urjbj.execute-api.ap-northeast-2.amazonaws.com']
+ALLOWED_HOSTS = ['w0r94urjbj.execute-api.ap-northeast-2.amazonaws.com', 's4ed5cxd42.execute-api.ap-northeast-2.amazonaws.com']
