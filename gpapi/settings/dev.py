46b9adff518ff94e2fba372cb384ed69a4c4d5ff
minhochoi <yovnthf1@naver.com>
2018-02-20 17:35:24 +0900

    Edit static path when deploying

--- gpapi/settings/dev.py
+++ gpapi/settings/dev.py
@@ -2,3 +2,5 @@ from gpapi.settings.base import *
 
 
 ALLOWED_HOSTS = ['w0r94urjbj.execute-api.ap-northeast-2.amazonaws.com', 's4ed5cxd42.execute-api.ap-northeast-2.amazonaws.com']
+
+STATIC_URL = '/dev/static/'
