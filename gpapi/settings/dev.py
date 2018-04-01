ed11a8ba3fb9d6408f5741abf1f94edf6fb62e52
qodot <waitingforqodot@gmail.com>
2018-04-01 11:49:38 +0900

    Fix ALLOWED_HOSTS

--- gpapi/settings/dev.py
+++ gpapi/settings/dev.py
@@ -1,6 +1,6 @@
 from gpapi.settings.base import *
 
 
-ALLOWED_HOSTS = ['w0r94urjbj.execute-api.ap-northeast-2.amazonaws.com', 's4ed5cxd42.execute-api.ap-northeast-2.amazonaws.com']
+ALLOWED_HOSTS = ['kzyv5zrz2c.execute-api.ap-northeast-2.amazonaws.com', 's4ed5cxd42.execute-api.ap-northeast-2.amazonaws.com']
 
 STATIC_URL = '/dev/static/'
